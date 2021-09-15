cb381d790a59d1f718579459b481d2d6d110e01b
MattBCM <matbcm01@gmail.com>
2021-09-15 14:25:37 -0400

    -got the add function to work with an example

--- Project1/Polynomial.java
+++ Project1/Polynomial.java
@@ -1,28 +1,54 @@
 public class Polynomial{
-    private double coeff[];
+    private double[] coeff;
     private int deg;
 
-    public Polynomial(double c[]){
+    public Polynomial(double[] c){
         deg = c.length;
         coeff = c;
     }
-    public Polynomial(double c[], int d){
+    public Polynomial(double[] c, int d){
         if(d < 0) throw new IllegalArgumentException("Degree must be positive");
         deg = d;
         coeff = c;
     }
-
+/*
     public evaluate(){
 
     }
-
-    public add(Polynomial other){
-        for(int i = this.deg; i >= 0; i++){
-            this.coeff[i] = this.coeff[i] + other.coeff[i]
+*/
+    public void add(Polynomial other){
+        if(this.deg >= other.deg) {
+            for (int i = other.deg; i > 0; i--) {
+                this.coeff[i - 1] += other.coeff[i - 1];
+            }
+        }
+        else {
+            double[] temp = new double[other.deg];
+            for(int i = other.deg; i > 0; i--){
+                if(i > (this.deg)) temp[i-1] = other.coeff[i-1];
+                else temp[i-1] = this.coeff[i-1] + other.coeff[i-1];
+            }
+            this.coeff = temp;
+            this.deg = other.deg;
         }
     }
 
 
+    public double[] getCoeff() {
+        return coeff;
+    }
+
+    public int getDeg() {
+        return deg;
+    }
+
+    public void setCoeff(double[] coeff) {
+        this.coeff = coeff;
+    }
+
+    public void setDeg(int deg) {
+        this.deg = deg;
+    }
 }
 /*
 public Polynomial sum(Polynomial p, Polynomialq){
--- Project1/main.java
+++ Project1/main.java
@@ -1,5 +1,9 @@
 public class main {
     public static void main(String[] args) {
-        Polynomial q()
+        double[] arr = {1, 2, 3, 4, 5};
+        double[] arr2 = {6, 5, 8, 3, 5, 8, 7};
+        Polynomial p = new Polynomial(arr);
+        Polynomial q = new Polynomial(arr2);
+        p.add(q);
     }
 }
